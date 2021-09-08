82be8ae3f03aa1748665027cc8d5b7c4f659774e
jayant13o6 <89663357+jayant13o6@users.noreply.github.com>
2021-09-09 02:37:17 +0530

    token variable added in schema

--- users.js
+++ users.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = new Schema({
     Username :{
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     Password:{
         type: String,
@@ -13,8 +16,24 @@ const userSchema = new Schema({
     Confirm_password: {
         type: String,
         required: true
-    }
+    },
+    tokens:[{ token:{type: String, required:true}}]
 },{timestamps:true});
+
+
+userSchema.methods.createToken = async function(){
+    try{
+        // const token = jwt.sign({Username:this.Username},"verystrongsecrettokeep")
+        // users.tokens = token.toString()
+        console.log(this._id)
+        const tokenx = jwt.sign({_id:this._id},"verystrongsecrettokeep")
+        console.log(tokenx)
+        this.tokens= this.tokens.concat({token:tokenx.toString()})
+        await this.save();
+        return tokenx;
+    }
+    catch(error){console.log(error);}
+}
 
 //models--> mongoose.model('collection name', 'schema name')
 const Users = mongoose.model('Users',userSchema);
